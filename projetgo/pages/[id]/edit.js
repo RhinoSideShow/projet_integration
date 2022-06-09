d189680a3e0f365e8f78077ec813e9d15a98314d
RhinoSideShow <Provencal.arnaud@gmail.com>
2022-06-09 13:06:14 -0400

    Debut css edit.js

--- projetgo/pages/[id]/edit.js
+++ projetgo/pages/[id]/edit.js
@@ -1,35 +1,50 @@
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 import useSWR from 'swr'
 import Form from '../../components/Form'
+import styles from "../../styles/Home.module.css";
+import Navbar from "../../Components/Navbar";
 
 const fetcher = (url) =>
-  fetch(url)
-    .then((res) => res.json())
-    .then((json) => json.data)
+    fetch(url)
+        .then((res) => res.json())
+        .then((json) => json.data)
 
 const EditMembre = () => {
-  const router = useRouter()
-  const { id } = router.query
-  const { data: membre, error } = useSWR(id ? `/api/membres/${id}` : null, fetcher)
+    const router = useRouter()
+    const {id} = router.query
+    const {data: membre, error} = useSWR(id ? `/api/membres/${id}` : null, fetcher)
 
-  if (error) return <p>Failed to load</p>
-  if (!membre) return <p>Loading...</p>
+    if (error) return <p>Failed to load</p>
+    if (!membre) return <p>Loading...</p>
 
-  const membreForm = {
-    _prenom: membre._prenom,
-    _nom: membre._nom,
-    _email: membre._email,
-    _tel: membre._tel,
-    _adresse: membre._adresse,
-    _benevole: membre._benevole,
-    _pw: membre._pw,
-    _status_adhesion: membre._status_adhesion,
-    _date_adhesion: membre._date_adhesion,
-    _admin:membre._admin,
+    const membreForm = {
+        _prenom: membre._prenom,
+        _nom: membre._nom,
+        _email: membre._email,
+        _tel: membre._tel,
+        _adresse: membre._adresse,
+        _benevole: membre._benevole,
+        _pw: membre._pw,
+        _status_adhesion: membre._status_adhesion,
+        _date_adhesion: membre._date_adhesion,
+        _admin: membre._admin,
 
-  }
+    }
 
-  return <Form formId="edit-membre-form" membreForm={membreForm} forNewMembre={false} />
+    return (
+        <div id="__next" className={styles.DivContainerHome}>
+            <Navbar/>
+            <div className={styles.DivContainer}>
+                <div className={styles.DivSousContainer}>
+
+                    <div className={styles.DivSousSousContainerSignIn}>
+                        <Form formId="edit-membre-form" membreForm={membreForm} forNewMembre={false}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
 }
 
 export default EditMembre
